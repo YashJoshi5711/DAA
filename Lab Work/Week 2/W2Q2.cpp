77558c205e38d097a8bbc963491c9f512cd6ffd9
Yash Joshi <84569413+YashJoshi5711@users.noreply.github.com>
2021-11-17 15:41:22 +0530

    Update W2Q2.cpp

--- Lab Work/Week 2/W2Q2.cpp
+++ Lab Work/Week 2/W2Q2.cpp
@@ -1,6 +1,28 @@
-//Given a sorted array of positive integers, design an algorithm and implement it using a program to find three indices i, j, k such that arr[i] + arr[j] = arr[k].
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
+
+bool findIndex(int arr[],int n,int x,int ind)
+{
+    int i=0,j=n-1,mid;
+    while(i<j)
+    {
+        if(arr[i]+arr[j]==x)
+        {
+            cout<<i+1<<" , "<<j+1<<" , "<<ind+1<<endl; 
+            return false;
+        }
+        if(arr[i]+arr[j]>x)
+        {
+            j-=1;
+        }
+        else
+        {
+            i+=1;
+        }
+    }
+    return true; 
+}
+
 int main()
 {
     int t;
@@ -9,33 +31,24 @@ int main()
     {
         int n;
         cin>>n;
-        vector<int> arr(n);
-        for(int &i:arr)
-            cin>>i;
-        int flag=0;
-        int i,j,k;
-        for(i=0;i<n-2;i++)
+        int arr[n];
+        for(int i=0;i<=n-1;i++)
         {
-            for(j=i+1;j<n-1;j++)
-            {    
-                for(k=j+1;k<n;k++)
-                {
-                    if(arr[i]+arr[j]==arr[k])
-                    {
-                        flag=1;
-                        break;
-                    }
-                }
-                if(flag)
-                    break;
-            }
-            if(flag)
-                break;
+            cin>>arr[i];
         }
-        if(flag)
-            cout<<i+1<<", "<<j+1<<", "<<k+1<<endl;
-        else
-            cout<<"No sequence found."<<endl;
+        int i=0;
+        while(i!=n)
+        {
+            if(!findIndex(arr,n,arr[i],i))
+            {
+                break;
+            }
+            i+=1;
+        }
+        if(i==n)
+        {
+            cout<<"No Sequence Found"<<endl;
+        }
     }
     return 0;
 }
