463569472cc2ef86a4562b08f0b72cc3b9c16071
Yash Joshi <84569413+YashJoshi5711@users.noreply.github.com>
2021-11-17 15:55:59 +0530

    Update W4Q3.cpp

--- Lab Work/Week 4/W4Q3.cpp
+++ Lab Work/Week 4/W4Q3.cpp
@@ -1,6 +1,71 @@
-//Given an unsorted array of integers, design an algorithm and implement it using a program to find Kth smallest or largest element in the array. (Worst case Time Complexity = O(n))
-#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<climits>
+  
 using namespace std;
+  
+int partition(int arr[], int l, int r, int k);
+  
+int findMedian(int arr[], int n)
+{
+    sort(arr, arr+n);
+    return arr[n/2]; 
+}
+  
+int kthSmallest(int arr[], int l, int r, int k)
+{
+    if (k > 0 && k <= r - l + 1)
+    {
+        int n = r-l+1; 
+        int i, median[(n+4)/5];
+        for (i=0; i<n/5; i++)
+            median[i] = findMedian(arr+l+i*5, 5);
+        if (i*5 < n) 
+        {
+            median[i] = findMedian(arr+l+i*5, n%5); 
+            i++;
+        }    
+        int medOfMed = (i == 1)? median[i-1]:kthSmallest(median, 0, i-1, i/2);
+        int pos = partition(arr, l, r, medOfMed);
+  
+        if (pos-l == k-1)
+            return arr[pos];
+        if (pos-l > k-1) 
+            return kthSmallest(arr, l, pos-1, k);
+        return kthSmallest(arr, pos+1, r, k-pos+l-1);
+    }
+  
+    return INT_MAX;
+}
+  
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+  
+int partition(int arr[], int l, int r, int x)
+{
+    int i;
+    for (i=l; i<r; i++)
+        if (arr[i] == x)
+           break;
+    swap(&arr[i], &arr[r]);
+  
+    i = l;
+    for (int j = l; j <= r - 1; j++)
+    {
+        if (arr[j] <= x)
+        {
+            swap(&arr[i], &arr[j]);
+            i++;
+        }
+    }
+    swap(&arr[i], &arr[r]);
+    return i;
+}
+  
 int main()
 {
     int t;
@@ -10,19 +75,16 @@ int main()
         int n;
         cin>>n;
         int arr[n];
-        for(int i=0;i<n;i++)
+        for(int i=0;i<=n-1;i++)
+        {
             cin>>arr[i];
+        }
         int k;
         cin>>k;
-        priority_queue<int> p;
-        for(int i=0;i<n;i++)
-        {
-            p.push(arr[i]);
-            if(p.size()>k)
-                p.pop();
-        }
-        cout<<p.top()<<endl;
+        cout<<kthSmallest(arr, 0, n-1, k)<<endl;
+
     }
     return 0;
 }
+
 
